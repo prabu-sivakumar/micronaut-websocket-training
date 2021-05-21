874aa0a110b6089a14634406e2d86fe444781318
Prabu Varadharajalu Sivakumar <prabu-sivakumar>
2021-05-21 16:32:37 +0000

    Initial commit

---
+++ src/test/java/training/micronaut/MicronautWebsocketTrainingTest.java
@@ -0,0 +1,21 @@
+package training.micronaut;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class MicronautWebsocketTrainingTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
